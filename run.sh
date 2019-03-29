26505b319b68de767a5b2b95c91336871cba20ce
deagantest <48824737+deagantest@users.noreply.github.com>
2019-03-28 21:22:31 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,7 @@
+tmux new-session -d -s avatar-server 'cd ripple/avatar-server && ./avatar-server'
+tmux new-session -d -s lets 'cd ripple/lets && python3.6 lets.py'
+tmux new-session -d -s bancho 'cd ripple/pep.py && python3.6 pep.py'
+tmux new-session -d -s api 'cd ripple/rippleapi && ./rippleapi'
+tmux new-session -d -s hanayo 'cd ripple/hanayo && ./hanayo'
+echo Mikoto Servers: Online
+echo Have Fun Master~
