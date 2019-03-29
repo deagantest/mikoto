677282049fe61906551f7fe838323ebc1a8ddfd6
deagantest <48824737+deagantest@users.noreply.github.com>
2019-03-28 22:10:46 -0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@ tmux new-session -d -s lets 'cd ripple/lets && python3.6 lets.py'
 tmux new-session -d -s bancho 'cd ripple/pep.py && python3.6 pep.py'
 tmux new-session -d -s api 'cd ripple/rippleapi && ./rippleapi'
 tmux new-session -d -s hanayo 'cd ripple/hanayo && ./hanayo'
-echo Mikoto Servers: Online
-echo Have Fun Master~
+
